27f061f263e48463364486aabe132e11ae38ee20
majdalkilany <majdkilany92@gmail.com>
2023-06-08 18:18:33 +0300

    remove the console

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,7 +1,6 @@
 import { DateTime } from './luxon.js';
 
 const setTime = () => {
-  console.log(DateTime.now());
   const time = document.querySelector('#date');
   const now = DateTime.now();
   const date = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
